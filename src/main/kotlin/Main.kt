e1bff88fd43afda39cc263ef7b87a9785a52a635
Machalka <al1999dk@gmail.com>
2022-03-13 21:24:26 +0300

    its work!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,31 +17,113 @@ val sensors_queue = Gauge.build().name("sensor_queue_len").help("Len of sensors 
 val logs_queue = Gauge.build().name("logs_queue_len").help("Len of logs queue").register()
 val sensors_time = Histogram.build().name("sensors_time").help("sensors time").register()
 
+val loggingQueue = ArrayBlockingQueue<LoggerData>(1000, true)
+val sensorQueue = ArrayBlockingQueue<SensorData>(1000, true)
 
 fun main() = runBlocking { // this: CoroutineScope
+    runSystem()
+    println("DONE")
+}
+
+suspend fun runSystem() = coroutineScope {
     launch { // launch a new coroutine and continue
-        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
-        println("World!") // print after delay
+        logging(loggingQueue)
     }
-    println("Hello") // main coroutine continues while a previous one is delayed
+    launch {
+        host(sensorQueue, loggingQueue)
+    }
+    repeat(3) {
+        launch {
+            tempSensor(sensorQueue, 1)
+        }
+    }
+    launch {
+        delay(5000L)
+        println("Stop...")
+        sensors_stop = true
+        waitForQueue(loggingQueue, sensorQueue)
+        stop = true
+        println("End main")
+    }
+}
+
+suspend fun tempSensor(sq: ArrayBlockingQueue<SensorData>, id: Int, ping: Long = 1000, lifecicle:Int=-1){
+//    println("Start sensor")
+    val sensorQueue = sq
+    val id = id
+    val p = ping
+    var lc=lifecicle
+    sensors_count.inc()
+    while (!sensors_stop && lc!=0) {
+        sensorQueue.put(SensorData(id, Random.nextInt(1, 100)))
+        sensors_queue.inc()
+        lc-=1
+        delay(1000L)
+    }
+    sensors_count.dec()
+    println("Terning off sensor:$id")
+}
+
+suspend fun host(sq: ArrayBlockingQueue<SensorData>, lq: ArrayBlockingQueue<LoggerData>){
+//    println("Start host")
+    val sensorQueue = sq
+    val loggingQueue = lq
+    var sd: SensorData
+    var lg: LoggerData
+    while (!stop) {
+        delay(1L)
+        if (sensorQueue.isEmpty()) {
+            continue
+        }
+        sd = sensorQueue.take()
+        sensors_queue.dec()
+        sd.freeze()
+        lg = LoggerData(sd, sensorQueue.count())
+        loggingQueue.put(lg)
+        logs_queue.inc()
+    }
+    println("Terning off host")
+}
+
+suspend fun logging(lq: ArrayBlockingQueue<LoggerData>){
+//    println("Start logger")
+    val loggingQueue = lq
+    val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+    val file = File("log.txt").bufferedWriter()
+    var lg: LoggerData
+    var logMassege: String
+    while (!stop) {
+        delay(1L)
+        if (loggingQueue.isEmpty()) {
+            continue
+        }
+        lg = loggingQueue.take()
+        logs_queue.dec()
+        lg.freeze()
+        logMassege =
+            "${lg.createLog(loggingQueue)} || ${sdf.format(Date())}\n"
+        print(logMassege)
+        file.write(logMassege)
+    }
+    file.close()
+    println("Terning off logger")
 }
 
 
 
+//fun stopAll(loggingQueue: ArrayBlockingQueue<LoggerData>, sensorQueue: ArrayBlockingQueue<SensorData>) {
+//    sensors_stop = true
+//    waitForQueue(loggingQueue, sensorQueue)
+//    stop = true
+//}
 
-private fun stopAll(loggingQueue: ArrayBlockingQueue<LoggerData>, sensorQueue: ArrayBlockingQueue<SensorData>) {
-    sensors_stop = true
-    waitForQueue(loggingQueue, sensorQueue)
-    stop = true
-}
-
-private fun waitForQueue(
+suspend fun waitForQueue(
     loggingQueue: ArrayBlockingQueue<LoggerData>,
     sensorQueue: ArrayBlockingQueue<SensorData>
 ) {
     while (loggingQueue.isNotEmpty() || sensorQueue.isNotEmpty()) {
         println("Waiting for queue to empty: loggingQueue=${loggingQueue.count()}  sensorQueue=${sensorQueue.count()}")
-        Thread.sleep(1000)
+        delay(1000L)
     }
 }
 
